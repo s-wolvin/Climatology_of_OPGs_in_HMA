e482dde776f25ac8540425f8596dceeb24ee1fe7
Savanna Wolvin <savanna.wolvin@gmail.com>
2022-07-22 12:37:22 -0600

    update script, add seasonal correlation

--- correlation/three_dim_corr_seasonal_atmos_VS_opgCoef.py
+++ correlation/three_dim_corr_seasonal_atmos_VS_opgCoef.py
@@ -4,7 +4,8 @@
     
 # SUMMARY
 # This script pulls an OPG coefficient and an atmospheric variable from the 
-# post-processed WRF output and finds the grid point of highest correlation.
+# post-processed WRF output and finds the grid point of highest correlation 
+# for each season (i.e., DJF, MAM, JJA, SON).
 
 # To run this script, you must compile finddatapoint.f90, which is a fortran 
 # script created to speed up the calculation of correlation, which can involve 
@@ -68,12 +69,12 @@ end_yr = 2015
 
 # Which quadratic variables to evaluate
 poly_fit = 'polyfit2'
-poly_var_num = 0
+poly_var_num = 1
 var_name = ['p1','p2','p3']
 
 # Atmospheric variable
-prdct_name = 'va'
-prdct_vars = 'v'
+prdct_name = 'wa'
+prdct_vars = 'w'
 
 # WRF output information
 wrf_nest = 'wrfout_d02'
@@ -104,9 +105,9 @@ else:
     prdct_dir = 'Z:/var_wrf/'
     facet_dir = 'Z:/wrf_facet_data/'
     save_dir = 'Z:/himat_opg_poly/'
+    
 
-
-
+        
 
 #%% load facets
 print('Load Facets and Date Vector...')
@@ -137,9 +138,9 @@ idx = 0
 for yr in years:
     print(str(yr))
     opg_fname = 'daily_opg_' + str(yr) + '_' + radius + 'km_' + poly_fit + '_faceted'
-    mat_file = sio.loadmat(opg_dir + opg_fname) # Load File
+    mat_file = sio.loadmat(opg_dir + opg_fname) # Load file
     
-    tx = mat_file['t'] # Pull Time
+    tx = mat_file['t'] # Pull time
     idx2 = idx + len(tx)
     
     poly_var[:,idx:idx2,:] = mat_file['opg_polyfit2'] # Pull OPG coefficients
@@ -154,7 +155,7 @@ poly_var = poly_var[poly_var_num,:,:] # Pull only one OPG coefficient
 #%% pull atmos var
 print('Pull Atmospheric Variable...')
 
-atmos_data = np.zeros((lay_size, np.shape(tref)[0], lat_size,lon_size), dtype=np.single)
+atmos_data = np.zeros((lay_size, np.shape(tref)[0], lat_size,lon_size))
 idx = 0
 
 if wrf_nest == 'wrfout_d03':
@@ -175,9 +176,8 @@ if wrf_nest == 'wrfout_d03':
         atmos_data[[2,3,4,5], idx:idx2,:,:] = mat_file[prdct_vars] # Save Var to Array
         
         idx = idx2
-
-    del mat_file
     
+    del mat_file
 elif wrf_nest == 'wrfout_d02':
     for yr in years:
         print(str(yr))
@@ -193,9 +193,25 @@ elif wrf_nest == 'wrfout_d02':
         idx = idx2
     
     del mat_file
-    
-    
-    
+
+
+
+
+
+
+#%% create array of indicies of each season
+print("Formulate seasonal vectors...")
+
+t_mont = tref.strftime('%m')
+t_mont = t_mont.to_list()
+
+djf = [idx for idx, elem in enumerate(t_mont) if (elem == '01' or elem == '02' or elem == '12')]
+mam = [idx for idx, elem in enumerate(t_mont) if (elem == '03' or elem == '04' or elem == '05')]
+jja = [idx for idx, elem in enumerate(t_mont) if (elem == '06' or elem == '07' or elem == '08')]
+son = [idx for idx, elem in enumerate(t_mont) if (elem == '09' or elem == '10' or elem == '11')]
+
+
+
 
 #%% calculate the correlations and the locations for each facet
 
@@ -205,73 +221,97 @@ lon_size = np.shape(atmos_data)[3]
 z_size = np.shape(atmos_data)[0]
 fi_size = np.shape(poly_var)[1]
 
+
 ### choose between new variables or to load up a previous variable
-location = np.zeros((fi_size, 3))
-corrCoef = np.zeros((fi_size, 1))
-linReg   = np.zeros((fi_size, 2))
-r_2      = np.zeros((fi_size, 1))
- 
-# mat_file = sio.loadmat(save_dir + 'three_dim_lin_regress_' + prdct_name )
+location = np.zeros((fi_size, 3, 4))
+corrCoef = np.zeros((fi_size, 4))
+linReg   = np.zeros((fi_size, 2, 4))
+r_2      = np.zeros((fi_size, 4))
+
+
+# mat_file = sio.loadmat(save_dir + 'three_dim_lin_regress_season_' + var_name[poly_var_num] + '_' + wrf_nest + '_' + prdct_name )
 # location = mat_file['location']
 # corrCoef = mat_file['corrCoef']
 # linReg   = mat_file['linReg']
 # r_2      = mat_file['r_2']
-# del mat_file
 
 
 ### loop through each facet
 print("Begin looping through facets...")
 for fi in np.arange(1,995+1):
+# for fi in helpme:
     print(str(fi))
     
-    ### Check if facet has enough grid points
-    if np.count_nonzero(facets == fi) > 2:
-        real_values = np.invert(np.isnan(poly_var[:,fi-1]))
-        t_size = np.count_nonzero(real_values)
+    ### if there is more than one datapoint on the facet, then continue
+    if np.count_nonzero(facets == fi) > 2:        
+        season = 0
         
-        ### Check if facet recieved more than 1 day of precipitation
-        if t_size > 1:
+        ### loop through each season
+        for sx in [djf, mam, jja, son]:
     
-            ### Find grid point with highest correlation to quadratic OPG coefficient
-            corrx = finddatapoint.corr_coef_point(fi, \
-                            atmos_data[:,real_values,:,:], poly_var[real_values,:], \
-                            lon_size, lat_size, z_size, t_size, fi_size)
+            real_values = np.invert(np.isnan(poly_var[:,fi-1]))
+            
+            ### create array of precip & season days
+            sx_days = np.zeros((len(tref)), dtype=bool) 
+            sx_days[sx] = True
+            timex = np.zeros((len(tref)), dtype=bool) 
+                          
+            for idx in range(len(real_values)-1): 
+                # pull dates with real values for the OPG coefficients and 
+                # dates within that season
+                timex[idx] = sx_days[idx] & real_values[idx] 
                 
-            # Find location of highest correlation
-            corrx = np.absolute(corrx)
-            max_corrx = np.nanmax(corrx)
-            idx = np.where(corrx == max_corrx)
-            corrCoef[fi-1,0] = max_corrx
-            
-            ### Pull coordinates of highest correlation
-            idx1 = idx[0]
-            idx2 = idx[1]
-            idx3 = idx[2]
-            
-            if len(idx1) == 1:
-                idx = np.concatenate(idx)
-            else:
-                idx = np.array([idx1[0], idx2[0], idx3[0]])
+            t_size = np.count_nonzero(timex)
 
-            ### save location in MATLAB indexing
-            location[fi-1, :] = idx + 1
+            ### if there is more than one precipitation day in that season, 
+            ### then continue
+            if t_size > 1:
+        
+                ### calculate correlation and location
+                corrx = finddatapoint.corr_coef_point(fi, \
+                            atmos_data[:,timex[:],:,:], poly_var[timex,:], \
+                                lon_size, lat_size, z_size, t_size, fi_size)
+              
+                ### remove infs
+                corrx[abs(corrx) == float('inf')] = float('nan')
+                
+                # Find location of highest correlation
+                corrx = np.absolute(corrx)
+                max_corrx = np.nanmax(corrx)
+                idx = np.where(corrx == max_corrx)
+                corrCoef[fi-1,0] = max_corrx
             
-            ### calculate linear regression
-            alx = poly_var[real_values, fi-1]
+                ### Pull coordinates of highest correlation
+                idx1 = idx[0]
+                idx2 = idx[1]
+                idx3 = idx[2]
+                
+                if len(idx1) == 1:
+                    idx = np.concatenate(idx)
+                else:
+                    idx = np.array([idx1[0], idx2[0], idx3[0]])
+                
+                ### save location in MATLAB indexing
+                location[fi-1, :, season] = idx + 1  
                     
-            px1 = atmos_data[idx[0], real_values, idx[1], idx[2]]
-            px1 = px1.reshape((-1,1))
+                ### calculate linear regression
+                alx = poly_var[timex, fi-1]
+
+                px1 = atmos_data[idx[0], timex, idx[1], idx[2]]
+                px1 = px1.reshape((-1,1))
+                
+                reg = LinearRegression().fit(px1,alx)
+
+                r_2[fi-1, season] = reg.score(px1, alx)
+                linReg[fi-1, 0, season] = reg.coef_
+                linReg[fi-1, 1, season] = reg.intercept_
                     
-            reg = LinearRegression().fit(px1,alx)
-            
-            r_2[fi-1, 0] = reg.score(px1, alx)
-            linReg[fi-1, 0] = reg.coef_
-            linReg[fi-1, 1] = reg.intercept_
-                    
+            season += 1
+
     # save/update .mat array everytime a facet's correlation is calculated
-    matfilename = 'three_dim_lin_regress_' + var_name[poly_var_num] + '_' + wrf_nest + '_' + prdct_name
+    matfilename = 'three_dim_lin_regress_season_' + var_name[poly_var_num] + '_' + wrf_nest + '_' + prdct_name 
     data = {'location':location,'linReg':linReg,'r_2':r_2,'corrCoef':corrCoef}                 
     sio.savemat(save_dir + matfilename + '.mat', data)                    
-    
 
+    
 
